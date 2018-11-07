a1ea0ca3dc9e0ca02505389fed63650ddaaa7bbd
Alexandre de Temmerman <alexandre2t@hotmail.fr>
2018-11-07 10:15:18 +0100

    del

--- mardi/app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ApplicationRecord
-	validates :email, presence: true
-	validates :email, uniqueness: true
-	validates :password, presence: true
-  has_many :users
-end
--- mardi/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  get '/', to: 'user#index'
-  get '/user/signup', to: 'user#signup'
-  get '/user/login', to: 'user#login'
-  resources 'user'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
